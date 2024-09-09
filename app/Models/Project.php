d7733306f723260380f0ef4c6a4f8f35c3e65808
yemannmyoe <yemannmyoe1876@gmail.com>
2024-09-10 00:43:35 +0700

    Prepare the database and Create Controllers and Define Routes

---
+++ app/Models/Project.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model // Corrected class name to PascalCase
+{
+    use HasFactory;
+
+    // Define the relationship: A project can have many tasks
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
